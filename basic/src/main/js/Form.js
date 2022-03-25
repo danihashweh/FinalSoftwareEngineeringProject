bff924ad56c2e0be72ecbe59aadbce76fb3d0ee8
krish <krishaneasparan@cmail.carleton.ca>
2022-03-25 01:49:34 -0400

    Added text questions.

--- basic/src/main/js/Form.js
+++ basic/src/main/js/Form.js
@@ -3,10 +3,41 @@ import Question from './Question'
 
 
 export default class Form extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {formValues: {}}
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+
+    const { questions } = this.props
+
+
+    if (prevProps.questions.length !== questions.length ) {
+      let formValues = {}
+      for(let question of questions) {
+        formValues[question.id] = ""
+      }
+      this.setState({formValues})
+    }
+
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const { formValues } = this.state
+    this.setState({ formValues: {...formValues, [name]: value}});
+  };
+
+
   render() {
+
+    const {formValues} = this.state
+
     const questionsArr = this.props?.questions ?? []
     const questions = questionsArr.map(question =>
-      <Question key={question.id} value={question.value} type={'text'}/>
+      <Question key={question.id} type={'text'} answer={formValues[question.id]} {...question}/>
     );
     return (
       <div>
--- basic/src/main/js/Question.js
+++ basic/src/main/js/Question.js
@@ -1,11 +1,40 @@
+import FormLabel from "@material-ui/core/FormLabel";
+import TextField from "@material-ui/core/TextField";
+
 const React = require('react'); // <1>
 
 export default class Question extends React.Component{
-  render() {
+
+  constructor(props) {
+    super(props);
+  }
+
+  renderTextQuestion() {
+    const { value, id, handleInputChange, answer } = this.props
     return (
-      <div>
-        <div>{this.props.value}</div>
-      </div>
+      <>
+        <FormLabel>{value}</FormLabel>
+        <TextField
+          id={id}
+          name={id}
+          type="string"
+          value={answer}
+          onChange={handleInputChange}
+        />
+      </>
     )
+  }
+
+  render() {
+    const { type } = this.props
+
+    switch(type) {
+      case "text":
+        return this.renderTextQuestion()
+      default:
+        return (
+            <div>{this.props.value}</div>
+        )
+    }
   }
 }
