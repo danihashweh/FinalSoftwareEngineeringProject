a3f81ecb4c277ea65decebca202a22c374a33f1c
krish <krishaneasparan@cmail.carleton.ca>
2022-03-25 02:15:45 -0400

    Fixed handling update to text answers.

--- basic/src/main/js/Question.js
+++ basic/src/main/js/Question.js
@@ -1,5 +1,6 @@
 import FormLabel from "@material-ui/core/FormLabel";
 import TextField from "@material-ui/core/TextField";
+import {Grid} from "@material-ui/core";
 
 const React = require('react'); // <1>
 
@@ -13,14 +14,16 @@ export default class Question extends React.Component{
     const { value, id, handleInputChange, answer } = this.props
     return (
       <>
-        <FormLabel>{value}</FormLabel>
-        <TextField
-          id={id}
-          name={id}
-          type="string"
-          value={answer}
-          onChange={handleInputChange}
-        />
+        <Grid item>
+          <FormLabel>{value}</FormLabel>
+          <TextField
+            id={id}
+            name={id}
+            type="string"
+            value={answer}
+            onChange={handleInputChange}
+          />
+        </Grid>
       </>
     )
   }
